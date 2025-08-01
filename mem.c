c7dcd164886264a390c2723701e7adbb5ec1dfc7
Kyle Sletten <kbsletten@gmail.com>
2025-07-31 23:34:34 -0600

    Use structs to simplify logic.

--- mem.c
+++ mem.c
@@ -5,7 +5,22 @@
 #include "mem.h"
 
 static
-size_t get_spec_size(const char **current, size_t *align, va_list *args);
+struct mem_info {
+	size_t size;
+	size_t align;
+} mem_zero;
+
+static
+struct field_info {
+	struct mem_info mem;
+	size_t count;
+} field_zero;
+
+static
+struct mem_info get_spec_size(const char **current, va_list *args);
+
+static
+struct field_info get_field_size(const char **current, va_list *args);
 
 static
 size_t round_up(size_t value, size_t alignment)
@@ -32,135 +47,132 @@ size_t get_field_type_size(char field_type)
 }
 
 static
-size_t get_field_size(const char **current, size_t *align, va_list *args)
+struct field_info get_array_size(const char **current, va_list *args)
 {
-	size_t field_size, field_count = 1, index_size = 0, item_align = 0;
+	struct field_info field;
+	size_t index_size = get_field_type_size(advance(current));
+
+	if (!index_size) {
+		return field_zero;
+	}
+
+	field = get_field_size(current, args);
+
+	return field;
+}
+
+static
+struct field_info get_field_size(const char **current, va_list *args)
+{
+	struct field_info field = { { 0, 0 }, 1 };
 	char field_type;
 
 	field_type = advance(current);
 
 	if (field_type & SPEC_MULT) {
-		field_count = 0;
+		field.count = 0;
 
 		while (**current & SPEC_MULT) {
-			field_count += advance(current) & 0x7F;
+			field.count += advance(current) & 0x7F;
 
-			if (field_count > SIZE_MAX >> 7) {
-				return 0;
+			if (field.count > SIZE_MAX >> 7) {
+				return field_zero;
 			}
 
-			field_count <<= 7;
+			field.count <<= 7;
 		}
 
-		field_count += advance(current);
+		field.count += advance(current);
 
-		if (!field_count) {
-			return 0;
+		if (!field.count) {
+			return field_zero;
 		}
 	}
 
 	switch (field_type & SPEC_TYPE) {
 		case SPEC_TYPE_DAT:
-			*align = field_size = get_field_type_size(field_type);
-
-			if (!field_size) {
-				return 0;
-			}
-
+			field.mem.align = field.mem.size = get_field_type_size(field_type);
 			break;
 
 		case SPEC_TYPE_STR:
 			if (get_field_type_size(field_type)) {
-				return 0;
+				return field_zero;
 			}
 
-			field_size = get_spec_size(current, align, args);
+			field.mem = get_spec_size(current, args);
 
 			if (advance(current)) {
-				return 0;
-			}
-			
-			if (!field_size) {
-				return 0;
+				return field_zero;
 			}
 			break;
 
 		case SPEC_TYPE_ARR:
-			index_size = get_field_type_size(field_type);
-
-			if (!index_size) {
-				return 0;
+			if (field.count > 1) {
+				return field_zero;
 			}
 
-			field_size = get_field_size(current, &item_align, args);
-
-			if (!field_size) {
-				return 0;
-			}
-
-			*align = item_align > index_size ? item_align : index_size;
-
-			return round_up(index_size, item_align) + field_size * field_count * va_arg(*args, size_t);
+			field = get_array_size(current, args);
+			break;
 
 		case SPEC_TYPE_PTR:
 			if ((field_type & SPEC_SIZE) != SPEC_SIZE_PTR) {
-				return 0;
+				return field_zero;
 			}
 
-			*align = field_size = sizeof(void *);
+			field.mem.align = field.mem.size = sizeof(void *);
 			break;
 
 		default:
-			return 0;
+			return field_zero;
 	}
 
-	return field_size * field_count;
+	if (!field.mem.size) {
+		return field_zero;
+	}
+
+	return field;
 }
 
 static
-size_t get_spec_size(const char **current, size_t *align, va_list *args)
+struct mem_info get_spec_size(const char **current, va_list *args)
 {
-	size_t size = 0, field_size, field_align = 0, length_fields = 0;
+	struct mem_info mem = mem_zero;
+	struct field_info field;
 
 	while (**current) {
-		field_size = get_field_size(current, &field_align, args);
+		field = get_field_size(current, args);
 
-		size = round_up(size, field_align) + field_size;
-		if (*align < field_align) {
-			*align = field_align;
+		mem.size = round_up(mem.size, field.mem.align);
+	       	mem.size += field.mem.size * field.count;
+		mem.size += (round_up(field.mem.size, field.mem.align) - field.mem.size) * (field.count - 1);
+		if (field.mem.align > mem.align) {
+			mem.align = field.mem.align;
 		}
 	}
 
-	return round_up(round_up(length_fields * sizeof(size_t), *align) + size, *align);
+	return mem;
 }
 
 size_t get_mem(const char *spec, void **ptr, size_t *align_ptr, ...)
 {
 	const char *current = spec;
-	size_t size = 0, align = 0;
+	struct mem_info mem;
 	va_list args;
 	va_start(args, align_ptr);
 
-	size = get_spec_size(&current, &align, &args);
+	mem = get_spec_size(&current, &args);
 
 	va_end(args);
 
-	if (!size) {
-		*ptr = NULL;
-		if (align_ptr) {
-			*align_ptr = 0;
-		}
+	if (!mem.size) {
 		return 0;
 	}
 
-	*ptr = malloc(size);
-
-	// TODO: set array lengths from va_args, locations and values will need to be calculated in get_spec_size.
-	// I need to think about how this will work with nested arrays.
+	*ptr = malloc(mem.size);
 
 	if (align_ptr) {
-		*align_ptr = align;
+		*align_ptr = mem.align;
 	}
 
-	return size;
+	return mem.size;
 }
